de2a300477440af914570f198f255c4d015d589c
laivanngoc <l.v.ngoc94@gmail.com>
2022-10-17 10:37:33 +0700

    sua bai tap ss1

---
+++ ss1/excercise1/class_table.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE class(
+id INT PRIMARY KEY,
+name varchar(40)
+);
+
---
+++ ss1/excercise1/teacher_table.sql
@@ -0,0 +1,8 @@
+DROP DATABASE teacher;
+use student_management;
+create table teacher(
+id int primary key,
+name varchar(40),
+age int,
+country varchar(40)
+);
